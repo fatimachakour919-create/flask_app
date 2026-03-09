44300a7be611e65fc9e4a78d5a7918e5a1d50362
fatimazhra <fatimachakour919@gmail.com>
2026-03-09 14:53:31 +0100

    commit2

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
 
-app =Flask(_name_)
+app =Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "hello devops - docker ci pipeline"
 
-if _name=="main_":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
